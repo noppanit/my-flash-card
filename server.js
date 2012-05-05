0bdf33280ed56a0829c14f4c9cca4f75fb2866dd
Noppanit Charassinvichai <noppanit.c@gmail.com>
2012-05-06 00:45:10 +0100

    Added json database

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 var express = require("express");
+var jade = require("jade");
+
+var database = { "vocab" : [
+		{
+			"id" : "0",
+			"german" : "Ich",
+			"english" : "I",
+		}, 
+		{
+			"id" : "1",
+			"german" : "Wurst",
+			"english" : "Sausage"
+		} 
+	]
+};
+
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-	response.send("Hello");
-})
+app.use(express.static(__dirname + "/assets"));
 
 var port = process.env.PORT || 3000;
+
 app.listen(port, function() {
 	console.log("Listening on ..." + port);
+});
+
+app.get('/', function(request, response) {
+	var randomNum = Math.floor(Math.random()*database.vocab.length);
+	var vocabulary = database.vocab[randomNum];
+	response.render("index.jade", {german: vocabulary.german, english: vocabulary.english});
 });
 
